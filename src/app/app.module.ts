9c9651caa122f8f68c6e3f9a2d08f910cc2aa652
Juston Bryant <justonbryant1@gmail.com>
2019-04-14 18:40:07 -0400

    Juston update food and navmenu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsermoodinfoComponent } from './usermoodinfo/usermoodinfo.component';
 import { VendorComponent } from './vendor/vendor.component';
+import { NavmenuComponent } from './navmenu/navmenu.component';
+import { FoodComponent } from './food/food.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsermoodinfoComponent,
-    VendorComponent
+    VendorComponent,
+    NavmenuComponent,
+    FoodComponent
   ],
   imports: [
     BrowserModule,
--- src/app/navmenu/navmenu.component.spec.ts
+++ src/app/navmenu/navmenu.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { NavigationMenuComponent } from './navigation-menu.component';
+import { NavmenuComponent } from './navmenu.component';
 
-describe('NavigationMenuComponent', () => {
-  let component: NavigationMenuComponent;
-  let fixture: ComponentFixture<NavigationMenuComponent>;
+describe('NavmenuComponent', () => {
+  let component: NavmenuComponent;
+  let fixture: ComponentFixture<NavmenuComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NavigationMenuComponent ]
+      declarations: [ NavmenuComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(NavigationMenuComponent);
+    fixture = TestBed.createComponent(NavmenuComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
